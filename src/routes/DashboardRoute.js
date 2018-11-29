93b89089e958bdd7124e54bf2e7a190bb5f1be95
Austine3000 <diribe3000@gmail.comgit config --global user.email diribe3000@gmail.com>
2018-11-29 10:38:35 +0100

    Added code splitting to increase profermance

--- src/routes/DashboardRoute.js
+++ src/routes/DashboardRoute.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Overview from '../containers/Overview/Overview';
+import asyncComponent from '../hoc/asyncComponent';
+
+const AsyncOverview = asyncComponent(() => {
+  return import('../containers/Overview/Overview');
+});
 
 class DashboardRoute extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/dashboard/overview" component={Overview} />
+          <Route path="/dashboard/overview" component={AsyncOverview} />
           <Redirect to="/dashboard/overview" />
         </Switch>
       </div>
